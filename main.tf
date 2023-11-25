380300c55b7ac6162f9a8a920c98f7f53fe13e7a
Jires Tchuenteu <jtchuenteu@gmail.com>
2023-11-25 10:56:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  
-  access_key ="AKIAT3VHLTGCILVULFMZ"
-  secret_key ="ami-093467ec28ae4fe03"
   region  = "us-west-2"
+  access_key ="AKIAT3VHLTGCMY4J6CWJ"
+  secret_key ="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"
+  
 }
  
 
-resource "aws_instance" "name" {
+resource "aws_instance" "myec2" {
   ami = "ami-093467ec28ae4fe03"
   instance_type = "t2.micro"
   tags = {
